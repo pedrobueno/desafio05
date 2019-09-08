d407e0956f11918613fe22acde0ca0dfe36a0af1
Pedro Bueno <pedro@mentores.com.br>
2019-09-08 19:26:27 -0300

    Finalizando aula modulo05

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import { Link } from 'react-router-dom';
+import { Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
 
 import api from '../../services/api';
 
@@ -8,6 +10,18 @@ export default function Main() {
   const [newRepo, setNewRepo] = useState('');
   const [repositories, setRepositories] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const repos = localStorage.getItem('repositories');
+
+    if (repos) {
+      setRepositories(JSON.parse(repos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('repositories', JSON.stringify(repositories));
+  }, [repositories]);
 
   const handleInputChange = e => {
     setNewRepo(e.target.value);
@@ -27,6 +41,7 @@ export default function Main() {
     setNewRepo('');
     setLoading(false);
   };
+
   return (
     <Container>
       <h1>
@@ -50,6 +65,20 @@ export default function Main() {
           )}
         </SubmitButton>
       </Form>
+
+      <List>
+        {repositories.map(repo => (
+          <li key={repo.name}>
+            <span>{repo.name}</span>
+            <Link
+              to={`/repository/${encodeURIComponent(repo.name)}`}
+              alt="link"
+            >
+              Detalhes
+            </Link>
+          </li>
+        ))}
+      </List>
     </Container>
   );
 }
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+import Container from '../../components/Container';
+import { Loadding, Owner, IssueList } from './styles';
+
+function Repository({ match }) {
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getRepo() {
+      setLoading(true);
+      const repoName = decodeURIComponent(match.params.name);
+
+      const [_repository, _issues] = await Promise.all([
+        api.get(`/repos/${repoName}`),
+        api.get(`/repos/${repoName}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepository(_repository.data);
+      setIssues(_issues.data);
+      setLoading(false);
+    }
+
+    getRepo();
+  }, []);
+
+  if (loading) {
+    return <Loadding>Carregando</Loadding>;
+  }
+
+  const { owner } = repository;
+  return (
+    <Container>
+      <Owner>
+        <Link to="/">Voltar</Link>
+        <img src={owner.avatar_url} alt={owner.login} />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+      <IssueList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={issue.html_url}
+                >
+                  {issue.title}
+                </a>
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssueList>
+    </Container>
+  );
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      name: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default Repository;
