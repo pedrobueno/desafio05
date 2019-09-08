0ceafd7b291cc8d05423b66641c57a296fb3d6a3
Pedro Bueno <pedro@mentores.com.br>
2019-09-08 20:23:13 -0300

    Updates

---
+++ precache-manifest.1cb0b37eb891b1bce5e938918a02b54b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9f957e3dbb740999a80419a88e06fa",
+    "url": "/desafio05/index.html"
+  },
+  {
+    "revision": "b1140ffcecbb70a00363",
+    "url": "/desafio05/static/js/2.260c2f19.chunk.js"
+  },
+  {
+    "revision": "0ccc78588d86cd46394c",
+    "url": "/desafio05/static/js/main.4c26071a.chunk.js"
+  },
+  {
+    "revision": "5eb41e6736932f7bfc4a",
+    "url": "/desafio05/static/js/runtime~main.54e2a6be.js"
+  }
+]);
